ce579fc91271e0da21d440c560e6621e3aa45daf
ultronubis <noname@example.com>
2024-04-06 11:23:33 -0400

    Removing duplicate calls when clearing surfaces

--- TOMB4/specific/winmain.cpp
+++ TOMB4/specific/winmain.cpp
@@ -423,10 +423,6 @@ void ClearSurfaces()
 	r.y2 = App.dx.rViewport.top + App.dx.rViewport.bottom;
 	r.x2 = App.dx.rViewport.left + App.dx.rViewport.right;
 
-	if (App.dx.Flags & DXF_HWR)
-		DXAttempt(App.dx.lpViewport->Clear2(1, &r, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0, 1.0F, 0));
-
-	S_DumpScreen();
 
 	if (App.dx.Flags & DXF_HWR)
 		DXAttempt(App.dx.lpViewport->Clear2(1, &r, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0, 1.0F, 0));
